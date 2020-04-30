863337e4c2d367bb74e26210375a7b224684d550
mkilmer <marcoskilmer1@gmail.com>
2020-04-30 18:50:45 -0300

    Add Animation When Open View

--- AppStoreClone/View/Today/TodayModal.swift
+++ AppStoreClone/View/Today/TodayModal.swift
@@ -11,11 +11,15 @@ import UIKit
 class TodayModal: UIViewController {
     var uiview:UIView?
     var frame:CGRect?
+    var topAnchor:NSLayoutConstraint?
+    var leadingAnchor:NSLayoutConstraint?
+    var heightAnchor:NSLayoutConstraint?
+    var widthAnchor:NSLayoutConstraint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .clear
         self.uiview = UIView()
     }
     
@@ -24,9 +28,45 @@ class TodayModal: UIViewController {
         guard let frame = self.frame else{return}
         
         uiview.backgroundColor = .blue
-        uiview.frame = frame
-        
+        uiview.layer.cornerRadius = 16
+        uiview.clipsToBounds = true
+       
         view.addSubview(uiview)
+        insertAutoLayoutProperties(uiview, frame)
+        uiViewAnimate()
+    }
+    
+    
+    func insertAutoLayoutProperties(_ uiview:UIView, _ frame:CGRect){
+        uiview.translatesAutoresizingMaskIntoConstraints = false
+        self.topAnchor = uiview.topAnchor.constraint(equalTo: view.topAnchor, constant: frame.origin.y)
+        self.leadingAnchor  = uiview.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: frame.origin.x)
+        self.heightAnchor = uiview.heightAnchor.constraint(equalToConstant: frame.height)
+        self.widthAnchor = uiview.widthAnchor.constraint(equalToConstant: frame.width)
+        
+        self.topAnchor?.isActive = true
+        self.leadingAnchor?.isActive = true
+        self.heightAnchor?.isActive = true
+        self.widthAnchor?.isActive = true
+        
+        view.layoutIfNeeded()
+        
        
     }
+    
+    func uiViewAnimate(){
+        
+        UIView.animate(withDuration: 0.3, delay: 0.0, options: .overrideInheritedCurve, animations: {
+            self.topAnchor?.constant = 0
+            self.leadingAnchor?.constant = 0
+            self.heightAnchor?.constant = self.view.frame.height
+            self.widthAnchor?.constant = self.view.frame.width
+            
+            self.uiview?.layer.cornerRadius = 0
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+    }
+    
+    
 }
